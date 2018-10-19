b52ff0fb45175057323888562a5ab83094a5ce00
Natalia Rodriguez <32943728+nrodriguez99@users.noreply.github.com>
2018-10-19 01:48:42 -0600

    la z y la l pra pra pra

---
+++ KillerSudoku2/Functions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillerSudoku2
+{
+    class Functions
+    {
+        int[,] cuadricula = new int[9, 9];
+        public static string str1, result;
+
+        public static void Initializer(ref int[,] cuadricula)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    cuadricula[i, j] = (i * 3 + i / 3 + j) % 9 + 1;
+                }
+            }
+        }
+
+        public static void CreateCuadricula(ref int[,] cuadricula, out string str2)
+        {
+            for(int x = 0; x < 9; x++)
+            {
+                for(int y = 0; y < 9; y++)
+                {
+                    str1 += cuadricula[x, y].ToString() + " ";
+                }
+                str1 += Environment.NewLine;
+            }
+            int[] xx = str1.Split(new[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string yy = "-----------" + Environment.NewLine + "| ";
+            for (int m = 0; m < xx.Length; m++)
+            {
+                yy += xx[m] + " ";
+                if(m % 3 == 2)
+                {
+                    yy += "| ";
+                }
+                if(m % 9 == 8 && m < xx.Length - 1 && m != 26 && m!= 53)
+                {
+                    yy += Environment.NewLine + "| ";
+                }
+                else if (m == 26 | m == 53)
+                {
+                    yy += Environment.NewLine + "-------------" + Environment.NewLine + "| ";
+                }
+                else if(m == xx.Length - 1)
+                {
+                    yy += Environment.NewLine + "-------------" + Environment.NewLine;
+                }
+            }
+            result = yy;
+            str2 = yy;
+            str1 = string.Empty;
+        }
+        public static void ChangeTwoCells(ref int[,] cuadricula, int value1, int value2)
+        {
+            int x1 = 0;
+            int y1 = 0;
+            int x2 = 0;
+            int y2 = 0;
+            for (int i = 0; i < 9; i+= 3)
+            {
+                for (int k = 0; k < 0; k+=3)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        for (int z = 0; z < 3; z++)
+                        {
+                            if(cuadricula[i + j, k + z] == value1)
+                            {
+                                x1 = i + j;
+                                y1 = k + z;
+                            }
+                            if (cuadricula[i + j, k + z] == value2)
+                            {
+                                x2 = i + j;
+                                y2 = k + z;
+                            }
+                        }
+                    }
+                    cuadricula[x1, y1] = value2;
+                    cuadricula[x2, y2] = value1;
+                }
+            }
+        }
+
+        public static void Actualizar(ref int[,] cuadricula, int generarCuadricula)
+        {
+            for(int cont = 0; cont < generarCuadricula; cont ++)
+            {
+                var rnd1 = new Random(Guid.NewGuid().GetHashCode());
+                var rnd2 = new Random(Guid.NewGuid().GetHashCode());
+                ChangeTwoCells(ref cuadricula, rnd1.Next(1,9), rnd2.Next(1,9));
+            }
+        }
+    }
+}
---
+++ KillerSudoku2/Matrix.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillerSudoku2
+{
+    class Matrix
+    {
+        public char[,] matrix;
+
+        public Matrix(int tamano)
+        {
+            this.matrix = new char[tamano, tamano];
+        }
+
+        public void generateMatrix()
+        {
+            List<int> numeros;
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    numeros = new List<int>();
+                    if (matrix[i, j] == ' ')
+                    {
+                        Boolean bandera = true;
+                        while(bandera)
+                        {
+                            Random rand = new Random();
+                            int numRand = rand.Next(1, 7);
+                            //Square root
+                            if(numRand == 1)
+                            {
+                                if(validateSquare(i,j))
+                                {
+                                    bandera = false;
+                                    break;
+                                }
+                            }
+                            //Rashita de perico *sniffs*
+                            else if(numRand == 2)
+                            {
+                                if(validateLine(i,j))
+                                {
+                                    bandera = false;
+                                    break;
+                                }
+                            }
+                            //La Z
+                            else if(numRand == 3)
+                            {
+                                if(validateZ(i,j))
+                                {
+                                    bandera = false;
+                                    break;
+                                }
+                            }
+                            //Y la L
+                            else if(numRand == 5)
+                            {
+                                if(validateL(i,j))
+                                {
+                                    bandera = false;
+                                    break;
+                                }
+                            }
+                            //La T de Troy
+                            else if(numRand == 5)
+                            {
+                                if(validateT(i,j))
+                                {
+                                    bandera = false;
+                                    break;
+                                }
+                            }
+                            //El punto guanacasteco
+                            else if(numRand == 6)
+                            {
+                                if((!validateSquare(i,j)) && (!validateLine(i,j)) && (!validateZ(i,j)) && (!validateL(i,j)) && (!validateT(i,j)))
+                                {
+                                    matrix[i, j] = 'p';
+                                    bandera = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public Boolean validateSquare(int i, int j)
+        {
+            Boolean banderaC = false;
+            if((matrix[i, j] == ' ') && (matrix[i, j + 1] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 1, j + 1] == ' '))
+                                        {
+                matrix[i, j] = 'c';
+                matrix[i, j + 1] = 'c';
+                matrix[i + 1, j] = 'c';
+                matrix[i + 1, j + 1] = 'c';
+                banderaC = true;
+            }
+            return banderaC;
+        }
+
+        public Boolean validateLine(int i, int j)
+        {
+            //NOTA: R de rashita, porque L de line ya existe en La L
+            Boolean banderaR = false;
+            if ((matrix[i, j] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 2, j] == ' ') && (matrix[i + 3, j] == ' '))
+            {
+                matrix[i, j] = 'r';
+                matrix[i + 1, j] = 'r';
+                matrix[i + 2, j] = 'r';
+                matrix[i + 3, j] = 'r';
+                banderaR = true;
+                return banderaR;
+            }
+            else if ((matrix[i, j] == ' ') && (matrix[i, j + 1] == ' ') && (matrix[i, j + 2] == ' ') && (matrix[i, j + 3] == ' '))
+            {
+                matrix[i, j] = 'r';
+                matrix[i, j + 1] = 'r';
+                matrix[i, j + 2] = 'r';
+                matrix[i, j + 3] = 'r';
+                banderaR = true;
+                return banderaR;
+            }
+            return banderaR;
+        }
+
+        public Boolean validateZ(int i, int j)
+        {
+            Boolean banderaZ = false;
+            if ((matrix[i, j] == ' ') && (matrix[i, j + 1] == ' ') && (matrix[i+1,j-1] == ' ') && (matrix[i+1,j] == ' '))
+            {
+                matrix[i, j] = 'z';
+                matrix[i, j + 1] = 'z';
+                matrix[i + 1, j - 1] = 'z';
+                matrix[i + 1, j] = 'z';
+                banderaZ = true;
+                return banderaZ;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i, j + 1] == ' ') && (matrix[i + 1, j + 1] == ' ') && (matrix[i + 1, j+2] == ' '))
+            {
+                matrix[i, j] = 'z';
+                matrix[i, j + 1] = 'z';
+                matrix[i + 1, j + 1] = 'z';
+                matrix[i + 1, j+2] = 'z';
+                banderaZ = true;
+                return banderaZ;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i+1, j] == ' ') && (matrix[i + 1, j + 1] == ' ') && (matrix[i + 2, j + 1] == ' '))
+            {
+                matrix[i, j] = 'z';
+                matrix[i+1, j] = 'z';
+                matrix[i + 1, j + 1] = 'z';
+                matrix[i + 2, j + 1] = 'z';
+                banderaZ = true;
+                return banderaZ;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i + 1, j-1] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 2, j - 1] == ' '))
+            {
+                matrix[i, j] = 'z';
+                matrix[i + 1, j-1] = 'z';
+                matrix[i + 1, j] = 'z';
+                matrix[i + 2, j - 1] = 'z';
+                banderaZ = true;
+                return banderaZ;
+            }
+            return banderaZ;
+        }
+
+        public Boolean validateT(int i, int j)
+        {
+            Boolean banderaT = false;
+            if((matrix[i,j] == ' ') && (matrix[i,j+1] == ' ') && (matrix[i,j+2] == ' ') && (matrix[i+1,j+1] == ' '))
+            {
+                matrix[i, j] = 't';
+                matrix[i, j + 1] = 't';
+                matrix[i, j+2] = 't';
+                matrix[i + 1, j+1] = 't';
+                banderaT = true;
+                return banderaT;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i+1, j] == ' ') && (matrix[i+2, j] == ' ') && (matrix[i + 1, j + 1] == ' '))
+            {
+                matrix[i, j] = 't';
+                matrix[i+1, j] = 't';
+                matrix[i+2, j] = 't';
+                matrix[i + 1, j + 1] = 't';
+                banderaT = true;
+                return banderaT;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i + 1, j-1] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 1, j + 1] == ' '))
+            {
+                matrix[i, j] = 't';
+                matrix[i + 1, j-1] = 't';
+                matrix[i + 1, j] = 't';
+                matrix[i + 1, j + 1] = 't';
+                banderaT = true;
+                return banderaT;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i + 1, j - 1] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 2, j] == ' '))
+            {
+                matrix[i, j] = 't';
+                matrix[i + 1, j - 1] = 't';
+                matrix[i + 1, j] = 't';
+                matrix[i + 2, j + 1] = 't';
+                banderaT = true;
+                return banderaT;
+            }
+            return banderaT;
+        }
+
+        public Boolean validateL(int i, int j)
+        {
+            Boolean banderaL = false;
+            if((matrix[i,j] == ' ') && (matrix[i+1, j] == ' ') && (matrix[i+2, j] == ' ') && (matrix[i+2, j+1] == ' '))
+            {
+                matrix[i, j] = 'l';
+                matrix[i+1, j] = 'l';
+                matrix[i+2, j] = 'l';
+                matrix[i+2, j+1] = 'l';
+                banderaL = true;
+                return banderaL;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 2, j] == ' ') && (matrix[i + 2, j - 1] == ' '))
+            {
+                matrix[i, j] = 'l';
+                matrix[i + 1, j] = 'l';
+                matrix[i + 2, j] = 'l';
+                matrix[i + 2, j - 1] = 'l';
+                banderaL = true;
+                return banderaL;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 1, j + 1] == ' ') && (matrix[i + 1, j + 2] == ' '))
+            {
+                matrix[i, j] = 'l';
+                matrix[i + 1, j] = 'l';
+                matrix[i + 1, j + 1] = 'l';
+                matrix[i + 1, j + 2] = 'l';
+                banderaL = true;
+                return banderaL;
+            }
+            else if((matrix[i, j] == ' ') && (matrix[i + 1, j] == ' ') && (matrix[i + 1, j - 1] == ' ') && (matrix[i + 1, j - 2] == ' '))
+            {
+                matrix[i, j] = 'l';
+                matrix[i + 1, j] = 'l';
+                matrix[i + 1, j - 1] = 'l';
+                matrix[i + 1, j - 2] = 'l';
+                banderaL = true;
+                return banderaL;
+            }
+            return banderaL;
+        }
+
+    }
+}
